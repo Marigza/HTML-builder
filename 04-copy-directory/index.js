33266ce269a55f4d0c7ebab94c01316bf6997ebb
Marigza <marigola@mail.ru>
2023-05-07 14:00:37 +0300

    feat: create copy function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs');
+path = require('path'); 
 
+
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
+  if (err) throw err;
+  fs.readdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err, cfiles) => {
+    if (err) throw err;
+    if (cfiles.length === 0) return;
+    for (const file of cfiles) {
+      fs.unlink(path.join(__dirname, 'files-copy', file), err => {
+        if (err) throw err;  
+      })
+    }
+  })  
+  fs.readdir(path.join(__dirname, 'files'), { recursive: true }, (err, files) => {
+    if (err) throw err;
+    for (const file of files) {
+      fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), err => {
+        if (err) throw err;
+      })
+    }     
+  })
+})
